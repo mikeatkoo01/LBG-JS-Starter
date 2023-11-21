e8c9c9f6213bd2bdda1345a1c3f7e3e64e643efa
unknown <mikeatkoo01@gmail.com>
2023-11-21 12:25:22 +0000

    javascript

---
+++ Javascript/02 arrays/Index.js
@@ -0,0 +1,21 @@
+let arr = ["mike", "amy","kian","alfie","beau-ivy"];
+
+console.log(arr);
+
+console.log(arr[3]);
+
+console.log(arr[1]);
+
+console.log(arr[4]);
+
+arr.push("luna")
+
+arr.pop("luna")
+
+num = [8]
+
+if () {
+    
+}
+
+

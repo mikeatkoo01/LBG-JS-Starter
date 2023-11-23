8a836ca5fa91b672d1808c333e5ed4287f7ecddb
unknown <mikeatkoo01@gmail.com>
2023-11-23 14:47:37 +0000

    MA js

--- 09_JavaScriptDOM/starter/index.js
+++ 09_JavaScriptDOM/starter/index.js
@@ -1,1 +1,37 @@
-const colours = ['red', 'green', 'blue', 'orange', 'lime','olive', 'black', 'yellow'];
+const colours = ['red', 'green', 'blue', 'orange', 'lime', 'olive', 'black', 'yellow'];
+
+const newParagraph = document.createElement("p");
+
+
+document.body.appendChild(newParagraph);
+
+const newheading = document.createElement("h1")
+
+document.body.appendChild(newheading);
+
+newheading.textContent = "First Heading";
+
+newheading.style.color = "red"
+
+newheading.style.color = "blue"
+
+
+
+
+const addTenParagraph = () => {
+
+    for (let i = 0; i <= 10; i++) {
+
+       let newpara = document.createElement("P")
+
+        newpara.textContent = `This is paragraph:${i}`;
+
+        document.body.appendChild(newpara)
+
+        newpara.style.color ="red"
+    }
+}
+
+
+// this is a loop for 10 paragraphs, This is paragraph:${i}`; temple litral
+
--- Javascript/functions/index.js
+++ Javascript/functions/index.js
@@ -1,36 +1,65 @@
 // function intros () {
-    
 
-{
-console.log("hello my name is mike");
+//  1 function / const 
+//  2 name the function 
+//  3( set the paramiters)
+//  4 => arrow marker 
+//  5 values and what they need to 
 
-console.log("hello my name is leo");
-}
 
 
-// this is to log this on the console
+// // this is to log this on the console
 
 function sum (num, num2) {
-    console.log("sum",num + num2);
-    
+    return num + num2;}
+
+// thid is to return the value instead of log the value
+const add = function (num, num2);
+    return (num + num2);
 }
 
 function sum2 (num, num2) {
-    console.log("sum2",num / num2);
-    
+    return num / num2;
+
 }
+const sum2 = function (num, num2) {
+    return (num / num2);
+}
+const div = (a, b) => a / b;
 
 function sum3 (num, num2) {
-    console.log("sum3",num * num2);
-    
+    return num * num2;
+
 }
+const sum3 = function (num, num2) {
+    return (num * num2);
+
+}
+
+const div2 = (a,b) => a * b;
+
+const math = (a,b) => a + b;
+
+const product = (a, b) => a * b;
+
 function sum4 (num, num2) {
-    console.log("sum4",num - num2);
-    
+    return num - num2;
+
+
+
+// then put in the numbers in the console
+
+function call(num) {
+    if (num % 3 === 0 && num % 5 === 0) {
+        return "FizzBuzz";
+    } else if (num % 3 === 0) {
+        return "Fizz";
+    } else if (num % 5 === 0) {
+        return "Buzz";
+    } else {
+        return num;
+    }
 }
-
-    
-
 
 
 
